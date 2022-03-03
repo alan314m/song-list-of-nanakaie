5841a0ab81e30066dfa944a13731e717c940735e
Anqi Chen <anqi.chen@g42.ai>
2022-03-03 16:16:52 +0400

    upgrade to version 1.1.0, immplemented custom scroll bar, added info stripe for development site

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Link from "next/link";
+
 import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/** <div style={{ textAlign: 'center', padding: '10px'}}>测试环境, 请尽量使用二级域名<Link href="https://www.7he.live" passHref>https://7he.live</Link>访问, 谢谢!</div> */}
       <Component {...pageProps} />
       <ToastContainer
         theme="light"
