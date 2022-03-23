dcb09a13f7d44294f95515c2dfedd6c1f58e2c19
Anqi Chen <anqi.chen@g42.ai>
2022-03-23 20:31:00 +0400

    updated song list 2022/3/23

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
 
 import { ToastContainer } from "react-toastify";
-
+//<div style={{ textAlign: 'center', padding: '10px'}}>测试环境, 请尽量使用二级域名<Link href="https://www.7he.live" passHref>https://7he.live</Link>访问, 谢谢!</div>
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/** <div style={{ textAlign: 'center', padding: '10px'}}>测试环境, 请尽量使用二级域名<Link href="https://www.7he.live" passHref>https://7he.live</Link>访问, 谢谢!</div> */}
       <Component {...pageProps} />
       <ToastContainer
         theme="light"
