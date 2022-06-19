3f7c191fdfc01c80b17e509a9c9dc5e1d515c047
Anqi Chen <anqi.chen@g42.ai>
2022-06-19 19:06:53 +0400

    modified merged parse code to fit actual use case

--- music_list_to_json.py
+++ music_list_to_json.py
@@ -1,28 +1,36 @@
 import json
 import pandas as pd
 import urllib.request
-import logging
-import time
+# import logging
+# import time
 song_list = []
 
 
 def getSongListUrl():
-    ## 改成对应主播的
-    url = "https://drive.kdocs.cn/api/v3/links/sn55vvgOpuyB/download?isblocks=false"  ## 把sn55vvgOpuyB改成对应主播的歌单尾缀，比如桃几的：https://www.kdocs.cn/l/sn55vvgOpuyB
-    req = urllib.request.Request(url, method="GET")
-    resp = urllib.request.urlopen(req)
-    jsonObjString = resp.read().decode('utf-8')
-
-    jsonObj = json.loads(jsonObjString)
-    if jsonObj["result"] != "ok":
-        logging.info("getSongListUrl err: fail to get download url")
-        # 这里可以加一个重试的逻辑哈
-    return jsonObj["fileinfo"]["url"]
+    url = "https://www.kdocs.cn/api/office/file/crNZPpbHA70t/download"
+    try:
+        req = urllib.request.Request(url, method="GET")
+        resp = urllib.request.urlopen(req)
+        jsonObjString = resp.read().decode('utf-8')
+        jsonObj = json.loads(jsonObjString)
+        return jsonObj["download_url"]
+    except:
+        print("Error obtaining song list URL")
+        exit()
+    
+    # if jsonObj["result"] != "ok":
+    #     logging.info("getSongListUrl err: fail to get download url")
+    #     # 这里可以加一个重试的逻辑哈
+    # return jsonObj["fileinfo"]["url"]
 
 
 def updateSongList():
     url = getSongListUrl()
-    urllib.request.urlretrieve(url, "歌单.xlsx")
+    try:
+        urllib.request.urlretrieve(url, "music_list_7.xlsx")
+    except:
+        print("Error retrieving the song list excel file")
+        exit()
 
 
 def parseSonglist():
@@ -30,7 +38,6 @@ def parseSonglist():
     song_df = song_df.where(pd.notnull(song_df), None)
     song_list.clear()
     for index, row in song_df.iterrows():
-        # print(index, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
         song_data = {"index": index, "song_name": row[0], "artist": row[1], "language": row[2], "remarks": row[3],
                      "initial": row[4], "sticky_top": row[5], "paid": row[6], "BVID": row[7]}
         if row[5] == 1:
@@ -41,9 +48,14 @@ def parseSonglist():
 
 if __name__ == '__main__':
     # 可以当个定时任务跑，最好多打点日志，不然坏了也不知道
-    while True:
-        updateSongList()
-        parseSonglist()
-        with open("./public/music_list_7.json", 'w') as file:
-            file.write(json.dumps(song_list))
-        time.sleep(60*60*24)
+    # while True:
+    #     updateSongList()
+    #     parseSonglist()
+    #     with open("./public/music_list_7.json", 'w') as file:
+    #         file.write(json.dumps(song_list))
+    #     time.sleep(60*60*24)
+
+    updateSongList()
+    parseSonglist()
+    with open("./public/music_list_7.json", 'w') as file:
+        file.write(json.dumps(song_list))
